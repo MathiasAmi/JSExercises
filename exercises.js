2a6b7bcfa30e55cd3f5d9546137ebc782c15ac37
Mathiasami <math35i0@edu.easj.dk>
2019-09-24 21:36:39 +0200

    exercise 1,2,3

--- exercises.js
+++ exercises.js
@@ -1,1 +1,57 @@
-console.log("hej");
+//Exercise 1
+console.log('Exercise 1:');
+
+let hashtag = '';
+
+for (let i = 0; i < 7; i++){
+    hashtag = hashtag + '#';
+    console.log(hashtag);
+}
+
+//Exercise 2:
+console.log("");
+console.log('Exercise 2:');
+
+for (i = 0; i < 100; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log(i + ' Fizzbuzz');
+    }
+    else if (i % 3 === 0){
+        console.log(i + ' Fizz');
+    }
+    else if( i % 5 === 0){
+        console.log(i + ' Buzz');
+    }
+    else {
+        console.log(i);
+    }
+}
+
+//Exercise 3. Looked at github. 
+console.log("");
+console.log('Exercise 3');
+
+var size = 8;
+var hash = '#';
+var space = ' ';
+
+for (var i = 1; i <= size; i++){
+    var line = '';
+
+    for (var j = 1; j <= size; j++){
+    if(i%2) {
+        if (j%2){
+            line = line + space;
+        } else {
+            line = line + hash;
+        }
+    } else {
+        if(j%2){
+        line = line + hash;
+    } else {
+        line = line + space;
+    }
+    }
+    }
+console.log(line);
+}
